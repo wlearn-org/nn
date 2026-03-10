88d0b4639f22d14972b8cb9c58e66c34d689a84f
zemlyansky <anton.zemlyansky@gmail.com>
2026-03-10 05:42:28 +0000

    refactor: add MLPModel, TabMModel, NAMModel unified classes via createModelClass

--- src/index.js
+++ src/index.js
@@ -1,8 +1,16 @@
 const { MLPClassifier, MLPRegressor } = require('./mlp.js')
 const { TabMClassifier, TabMRegressor } = require('./tabm.js')
 const { NAMClassifier, NAMRegressor } = require('./nam.js')
+const { createModelClass } = require('@wlearn/core')
+
+const MLPModel = createModelClass(MLPClassifier, MLPRegressor, { name: 'MLPModel' })
+const TabMModel = createModelClass(TabMClassifier, TabMRegressor, { name: 'TabMModel' })
+const NAMModel = createModelClass(NAMClassifier, NAMRegressor, { name: 'NAMModel' })
 
 module.exports = {
+  // Unified classes (recommended)
+  MLPModel, TabMModel, NAMModel,
+  // Original split classes (backward compat)
   MLPClassifier, MLPRegressor,
   TabMClassifier, TabMRegressor,
   NAMClassifier, NAMRegressor
